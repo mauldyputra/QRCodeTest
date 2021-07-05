a972948c1c839a308c6a10accbe9b83aa13233f3
Mauldy Putra <mauldyputra@gmail.com>
2021-07-05 16:40:06 +0700

    add new image preview vc

---
+++ QRScannerTest/QRScannerTest/ImagePreview.swift
@@ -0,0 +1,17 @@
+//
+//  ImagePreview.swift
+//  QRScannerTest
+//
+//  Created by Mauldy Putra on 05/07/21.
+//
+
+import UIKit
+
+class ImagePreview: UIViewController {
+
+    @IBOutlet weak var imagePreview: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
